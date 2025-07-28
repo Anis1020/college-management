3de2a92410f4ec4ee5886d0379a25ab30abe2b91
nexgensoft0 <nexgensoft0@gmail.com>
2025-07-28 09:25:45 +0600

    student interface added

---
+++ src/app/modules/student/interface.ts
@@ -0,0 +1,33 @@
+import { Types } from "mongoose";
+
+export type TName = {
+  firstName: string;
+  lastName: string;
+};
+export type TGuardian = {
+  name: TName;
+  relation: string;
+  contactNo: string;
+};
+export type TLocalGuardian = {
+  name: TName;
+  relation: string;
+  contactNo: string;
+  address: string;
+};
+export type TStudent = {
+  id: string;
+  user: Types.ObjectId;
+  name: TName;
+  gender: "male" | "female" | "other";
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: TGuardian;
+  localGuardian?: TLocalGuardian;
+  profileImage?: string;
+  academicDepartment: Types.ObjectId;
+  admissionSemester: Types.ObjectId;
+};
